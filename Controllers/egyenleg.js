178ad03251050b02a37e04e6f1db0b4c695a9cb8
bartekaron <78853724+bartekaron@users.noreply.github.com>
2023-11-25 16:07:10 +0100

    Egyenleg elkészítése és designolás

---
+++ Controllers/egyenleg.js
@@ -0,0 +1,59 @@
+function showChartEgyenleg(){
+    let labels = [];
+    let IncomeDatas = [];
+    let IssuenceDatas = [];
+
+  axios.get(`${serverURL}/items/userID/eq/${loggedUser.ID}`).then((res) => {
+    res.data.sort((a,b) => a.date.localeCompare(b.date));
+    res.data.forEach((item) => {
+      labels.push(item.date.toString().split("T")[0]);
+      
+      if(item.type.toLowerCase() == "bevétel"){
+        IncomeDatas.push({x: item.date.toString().split("T")[0], y: item.amount})
+        IssuenceDatas.push({x: item.date.toString().split("T")[0], y: 0})
+        
+      }
+      else{
+        IssuenceDatas.push({x: item.date.toString().split("T")[0], y: item.amount})
+        IncomeDatas.push({x: item.date.toString().split("T")[0], y: 0})
+      }
+      
+      //datas.push(item.amount);
+    });
+  });
+
+  setTimeout(() => {
+    const ctx = document.getElementById("myChartEgyenleg");
+
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Bevétel:",
+            data: IncomeDatas,
+            borderWidth: 3,
+          },
+
+          {
+            label: "Kiadás",
+            data: IssuenceDatas,
+            borderWidth: 3,
+          }
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: false
+          }
+        },
+      },
+    });
+  }, 500);
+}
